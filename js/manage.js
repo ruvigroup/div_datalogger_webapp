9a9b2a835e700c75c70726f16e4723fd30b5ae3b
feuerblitz7 <alexrasch@gmail.com>
2018-02-20 15:30:50 +0100

    Manage data update, start/stop recording fixed

--- js/manage.js
+++ js/manage.js
@@ -1,10 +1,11 @@
 // get HTML elements
 var recordButton = document.getElementById("recordButton");
 var recordingTimeSpan = document.getElementById("recordingTime");
-var recordButton = document.getElementById("shutdownButton");
+var shutdownButton = document.getElementById("shutdownButton");
 
 //declare global variables
 var ros,UserCMDService;
+var idRecordingTimeInterval;
 
 var SUCCESS = 0; //serviceReponse if the request succeeded
 
@@ -13,8 +14,8 @@ var REQ_REC_START = 1; // start recording
 var REQ_REC_STOP  = 2; // stop recording
 var REQ_SHUTDOWN  = 10; //shutdown RPi
 
-var STATUS_RECORD_ON  = 1; // recording on status (status_id response from the service)
-var STATUS_RECORD_OFF = 2; // recording off status (status_id response from the service)
+var STATUS_RECORD_ON  = 1; // recording on status (state_id response from the service)
+var STATUS_RECORD_OFF = 0; // recording off status (state_id response from the service)
 
 // create the requests for the service
 var statusRequest = new ROSLIB.ServiceRequest({
@@ -65,14 +66,17 @@ function toggle(){
 function setRecordOn(recordButton){
   UserCMDService.callService(startRecordRequest, function(response){
     if (response.msg_id == SUCCESS){
-      setRecordOnStyle(recordButton);
-    }
+      setRecordOnStyle(recordButton, response);
+}
   });
 }
 
-function setRecordOnStyle(recordButton){
+function setRecordOnStyle(recordButton, response){
   recordButton.children[1].innerHTML = 'Recording...';
   recordButton.className = 'button recordOn';
+idRecordingTimeInterval = setInterval(function(){
+    setRecordingTime(new Date().getTime() - response.rec_time.secs*1000 + Math.round(response.rec_time.nsecs/1000000));
+}, 1000);
 }
 
 // set record on, starts recording via the service and change the recordButton class if the request succeeded
@@ -87,6 +91,10 @@ function setRecordOff(recordButton){
 function setRecordOffStyle(recordButton){
   recordButton.children[1].innerHTML = 'Recording OFF';
   recordButton.className = 'button recordOff';
+	if (idRecordingTimeInterval != null){
+	 	clearInterval(idRecordingTimeInterval);
+	}
+setRecordingTime();
 }
 
 function sendShutdownRequest(){
@@ -96,7 +104,7 @@ function sendShutdownRequest(){
 }
 
 function setRecordingTime(msecs){
-  if (msecs == []){
+  if (msecs == null){
     recordingTimeSpan.innerHTML = 'Not recording.';
   } else {
     recordingTimeSpan.innerHTML = new Date(msecs).toISOString().substr(11, 8);
@@ -106,15 +114,15 @@ function setRecordingTime(msecs){
 ros = connectToROS();
 UserCMDService = connectToService('/user_cmd','div_datalogger/UserCMD');
 UserCMDService.callService(statusRequest, function(response){
-  console.log(response.rec_time + ' ' + response.msg_id + ' ' + response.status_id);
-  switch (response.status_id){
+  switch (response.state_id){
     case STATUS_RECORD_ON:
-    setRecordOnStyle(recordButton);
-    setRecordingTime(response.rec_time.secs*1000 + Math.round(response.rec_time.nsecs/1000) - new Date().getTime());
+    setRecordOnStyle(recordButton, response);
+    
     break;
     case STATUS_RECORD_OFF:
     setRecordOffStyle(recordButton);
-    setRecordingTime([]);
+    setRecordingTime();
+
     break;
   }
 });
